f674503eab3a198158d9d0b433bf9ab1c3eff626
KBeKind <klee@LAPTOP-5P7KDISB>
2023-10-10 17:33:41 -0500

    small update

--- src/components/UseEffectSample.tsx
+++ src/components/UseEffectSample.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 const UseEffectSample = () => {
   const [resourceType, setResourceType] = useState("type1");
+  const [items, setItems] = useState();
 
   useEffect(() => {
     //everything inside this useEffect function will be executed every time the application renders
@@ -14,6 +15,16 @@ const UseEffectSample = () => {
     // you can leave an empty array as the second argument
     // this will make it only run on mount
   }, []);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+    // this causes the fetch to run and print out whatever
+    // is fetched from the resource type when it is changed
+    // this will happen on load as well because resourceType starts as nothing
+    // this will only happen when the value of resourceType is changed
+  }, [resourceType]);
 
   return <div>UseEffectSample</div>;
 };
